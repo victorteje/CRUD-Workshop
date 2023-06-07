7b556ba5c3c55ba3229d26f6ae81fb47290907ff
victorteje <vteje2@gmail.com>
2023-06-07 14:34:19 +0200

    starting functions

---
+++ index.js
@@ -0,0 +1,20 @@
+const createItem = () => {
+    let item = document.getElementById("task").value;
+    console.log(item);
+}
+
+const readItem = () => {
+    
+}
+
+const updateItem = () => {
+    
+}
+
+const deleteItem = () => {
+    
+}
+
+const editItem = () => {
+    
+}
