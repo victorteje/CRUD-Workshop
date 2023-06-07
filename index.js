4683dc61ce6256b915a0a0978c313081dae9e373
victorteje <vteje2@gmail.com>
2023-06-07 15:17:19 +0200

    create button makes list

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const createItem = () => {
     let item = document.getElementById("task").value;
     console.log(item);
+    localStorage.setItem("created", item);
+    let pendingList = document.getElementById("pending");
+    let pendingTask = document.createElement("li");
+    pendingTask.innerHTML = item;
+    pendingList.appendChild(pendingTask);
 }
 
 const readItem = () => {
