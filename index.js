88916f81b5c1747522b3e296fad29c29f4bb53c8
victorteje <vteje2@gmail.com>
2023-06-09 21:41:20 +0200

    edit button functionality added

--- index.js
+++ index.js
@@ -9,21 +9,27 @@ const createItem = () => {
     pendingList.appendChild(pendingTask);
     let taskContainer = document.createElement("div");
     pendingTask.appendChild(taskContainer);
+
     let checkCompleted = document.createElement("input");
     checkCompleted.setAttribute("type", "checkbox");
     taskContainer.appendChild(checkCompleted);
+
     let taskText = document.createElement("input");
     taskText.setAttribute("type", "text");
     taskText.setAttribute("value", getItem);
     taskText.readOnly = true;
+    taskText.className = "taskInput"
     
     taskContainer.appendChild(taskText);
     let editTask = document.createElement("button");
     editTask.innerText = "Edit";
     editTask.className = "editButton";
     taskContainer.appendChild(editTask);
-    editTask.onclick = localStorage.removeItem(getItem);
-
+    // editTask.onclick = localStorage.removeItem(getItem);
+    editTask.addEventListener("click", editInput);
+    function editInput(){
+        taskText.readOnly = false;
+    }
 
     let deleteTask = document.createElement("button");
     deleteTask.innerText = "Delete";
